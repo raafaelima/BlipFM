678c8d0e2b96501f52be448371e9662ee170868a
Rafael Lima <rafael.lima2403@gmail.com>
2023-07-07 23:04:43 +0100

    First Commit

---
+++ BlipFM/Core/BlipFmApp.swift
@@ -0,0 +1,17 @@
+//
+//  BlipFmApp.swift
+//  BlipFM
+//
+//  Created by Lima, Rafael on 07/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct BlipFmApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
