5a12e3a44a1bdae7f0981dc46451026cf83bf730
Rafael Lima <rafael.lima2403@gmail.com>
2023-07-10 10:48:41 +0100

    RRefactor Album Details to Better Orgranizatoin

--- BlipFM/View/AlbumDetails/AlbumDetailsView.swift
+++ BlipFM/View/AlbumDetails/AlbumDetailsView.swift
@@ -12,41 +12,21 @@ struct AlbumDetailsView: View {
     @StateObject var viewModel: AlbumDetailsViewModel
 
     init(album: Album) {
-        let vm = AlbumDetailsViewModel(album: album)
-        self._viewModel = StateObject(wrappedValue: vm)
+        let albumDetailsVM = AlbumDetailsViewModel(album: album)
+        self._viewModel = StateObject(wrappedValue: albumDetailsVM)
     }
 
     var body: some View {
+
         VStack(spacing: 5) {
-
             HStack {
-                VStack(alignment: .leading) {
-                    Text(viewModel.albumDetail.name)
-                        .font(.title)
-                        .lineLimit(1)
-
-                    Text(viewModel.albumDetail.artist)
-                        .font(.title3)
-                        .lineLimit(1)
-                }
-
+                albumInfo()
                 Spacer()
-
                 albumImage(viewModel.albumDetail.albumCoverURL())
             }
             .padding(.horizontal)
 
-            VStack(alignment: .leading, spacing: 6) {
-                Text("\(viewModel.albumDetail.playcount.formatted()) scrobbles")
-                    .font(.headline)
-                    .foregroundColor(.gray)
-
-                Text("\(viewModel.albumDetail.listeners.formatted()) listeners")
-                    .font(.headline)
-                    .foregroundColor(.gray)
-            }
-            .padding()
-            .frame(maxWidth: .infinity, alignment: .leading)
+            listenersAndScrobles()
 
             if !viewModel.albumDetail.summary.isEmpty {
                 Text(viewModel.albumDetail.summary)
@@ -57,24 +37,7 @@ struct AlbumDetailsView: View {
                     .padding(.vertical, 5)
             }
 
-            VStack(alignment: .leading, spacing: 5) {
-                Text("Top Tracks")
-                    .font(.title)
-                    .padding(.horizontal)
-                    .padding(.top, 30)
-
-                List(viewModel.albumDetail.tracks) { track in
-                    VStack(alignment: .leading, spacing: 10) {
-                        Text(track.name)
-                            .font(.system(size: 16, weight: .bold))
-
-                        let trackDuration: Duration = .seconds(track.duration)
-                        Text("\(trackDuration, format: .time(pattern: .minuteSecond)) min")
-                            .font(.system(size: 14, weight: .light))
-                    }
-                }
-            }
-
+            topTracks()
         }
         .onAppear {
             viewModel.fetchAlbumDetails()
@@ -91,6 +54,56 @@ struct AlbumDetailsView: View {
         } placeholder: {
             Image(systemName: "photo.fill")
         }.frame(width: 100, height: 100)
+    }
+
+    private func albumInfo() -> some View {
+        VStack(alignment: .leading) {
+            Text(viewModel.albumDetail.name)
+                .font(.title)
+                .lineLimit(1)
+
+            Text(viewModel.albumDetail.artist)
+                .font(.title3)
+                .lineLimit(1)
+        }
+    }
+
+    private func listenersAndScrobles() -> some View {
+        VStack(alignment: .leading, spacing: 6) {
+            Text("\(viewModel.albumDetail.playcount.formatted()) scrobbles")
+                .font(.headline)
+                .foregroundColor(.gray)
+
+            Text("\(viewModel.albumDetail.listeners.formatted()) listeners")
+                .font(.headline)
+                .foregroundColor(.gray)
+        }
+        .padding()
+        .frame(maxWidth: .infinity, alignment: .leading)
+    }
+
+    private func topTracks() -> some View {
+        VStack(alignment: .leading, spacing: 5) {
+            Text("Top Tracks")
+                .font(.title)
+                .padding(.horizontal)
+                .padding(.top, 30)
+
+            List(viewModel.albumDetail.tracks) { track in
+               trackRow(track)
+            }
+        }
+    }
+
+    private func trackRow(_ track: Track) -> some View {
+        VStack(alignment: .leading, spacing: 10) {
+            Text(track.name)
+                .font(.system(size: 16, weight: .bold))
+
+            let trackDuration: Duration = .seconds(track.duration)
+            Text("\(trackDuration, format: .time(pattern: .minuteSecond)) min")
+                .font(.system(size: 14, weight: .light))
+        }
     }
 }
 
